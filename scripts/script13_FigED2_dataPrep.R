0312afdfdde36f3f1e32baf6862fe9b4d530b940
sheftneal <sheftneal@stanford.edu>
2020-06-29 14:00:11 -0700

    script 13/14 split
    improve split between processing and plotting

--- scripts/script13_FigED2_dataPrep.R
+++ scripts/script13_FigED2_dataPrep.R
@@ -109,21 +109,11 @@ bod_vand$r_aod <- summary(lm(bod ~ child_birth_year, data = bod_vand))$residuals
 
 
 
-
-
-# 
-# loc <- elsewhere[,c("lon","lat")]
-# loc <- loc[!duplicated(loc),]
-# 
-# aod_else <- dust[cellFromXY(grid, as.matrix(loc))]
-
-
-
 ################################        Work with different locations to get data ################################################################        
 
 
 
-
+#this is a stupid way to do this but just manually found lat lon for cities and pull in time series for each here
 
 bfs_cells <- get_cells(-2.416584, 13.579979 )
 data_full$bfs <- get_series(bfs_cells)
@@ -180,104 +170,5 @@ for(i in 1:length(cors)){
   cors[[i]]<-get_cor(as.numeric(data_full2[,nms1[i]]))
 }
 
-
-
-
-######### TEMPORAL FIGURE ############
-
-
-pdf("figures/raw/FigED2b_insert_raw.pdf",width = 6, height = 3.5)       
-par(mar = c(0,0,0,0))
-
-plot(africa, lwd = 0.01, col = 'gray90', border = 'gray30')
-points(xyFromCell(grid, bod_cells), pch = 15, col = 'red', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, nmy_cells)), pch = 15, col = 'forestgreen', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, kno_cells)), pch = 15, col = 'orange', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, nrb_cells)), pch = 15, col = 'navy', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, bfs_cells)), pch = 15, col = 'red3', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, bmk_cells)), pch = 15, col = 'royalblue', cex = 0.4)
-points(xyFromCell(grid, cellFromXY(grid, dkr_cells)), pch = 15, col = 'purple', cex = 0.4)
-dev.off()
-
-
-
-
-pdf("figures/raw/FigED2b_raw.pdf", width = 7.5, height = 5)      
-par(mar = c(5,5,4,4))    
-
-xy <- data.frame(x = -2:6, y1 = cors[["nmy"]], y2 = cors[["kno"]], y3 = cors[["mtn"]], y4 = cors[["nrb"]], y5 = cors[["dkr"]], y6 = cors[["kms"]], y7 = cors[["lagos"]], y8 = cors[["bmk"]], y9 = cors[["djo"]], y10 = cors[["bfs"]]) %>% filter(x>=0)
-
-plot(0:6, predict(loess(y1 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), type = "l", xlab = "", axes=F,ylab = "", ylim = c(-.025, 0.25), col = NA,lwd = 2)
-
-abline(h = seq(0, 0.25, 0.05), col = 'gray75', lwd = 0.75, lty = 4)
-abline(h = 0, lty = 4, col = 'black', lwd = 0.75)
-
-lines(0:6, predict(loess(y1 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'forestgreen', lwd = 2)
-lines(0:6, predict(loess(y2 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'orange', lwd = 2)
-lines(0:6, predict(loess(y4 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'navy', lwd = 2)
-lines(0:6, predict(loess(y10 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'red3', lwd = 2)
-lines(0:6, predict(loess(y8 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'royalblue', lwd = 2)
-lines(0:6, predict(loess(y5 ~ x, data = xy, span = 1), newvalues = data.frame(x = 0:6)), col = 'purple', lwd = 2)
-
-
-axis(1, at = 0:6, labels = c("day t", paste("day t+", 1:6, sep = "")))
-axis(2, tick = T, las =2)
-
-mtext(side = 1, text = "Lagged day",line = 3.5, cex =1.25)
-mtext(side = 2, text = "Daily correlation ",line = 3.5, cex = 1.25)
-
-dev.off()
-
-
-
-
-
-
-#######################
-
-
-#  dts gives date info for layers
-#   dust is raster brick   
-grid[] <- 1:ncell(grid)  
-
-africa_cells <- raster::extract(grid, africa, small = T) %>% unlist() %>% unique() %>% sort()
-all_cells <- 1:ncell(grid)
-non_africa_cells <- all_cells[all_cells %in% africa_cells == F]
-dust_backup <- dust
-
-dust[non_africa_cells] <- NA
-dust[dust>2.5]<-2.5 #ub
-dust[dust<0]<-0 #lb
-dust[1]<-3 #set a max
-dust[2]<-0 #set a min
-#now have range 0-3 for color scale
-
-
-
-
-
-pal1 <- colorRampPalette(colors = add.alpha(c("#404096","#63AD99","#BEBC48","#E66B33","#D92120"), 0.5))
-
-start_pt<- tp<-which(dts$year == 2009 & dts$month == 2 & dts$dom == 10)
-
-
-pdf("figures/raw/FigED2a_raw.pdf", width = 3, height = 4)
-par(mfrow = c(3,2))
-for(tp in (start_pt):(start_pt+5)){
-  par(mar = c(0,0,0,0))
-  plot(africa, border = NA)
-  plot(dust[[tp]], add = T, legend = F, col = pal1(256))
-  plot(africa, add=T, lwd = 0.1)
-  rect(xleft = -40, xright = -19, ybottom = -40, ytop = 40, border = NA, col ='white')
-  rect(xleft = -40, xright = 0, ybottom = -40, ytop = 0, border = NA, col ='white')
-  rect(xleft = 60, xright = 70, ybottom = -40, ytop = 40, border = NA, col ='white')
-  rect(xleft = -40, xright = 60, ybottom = -60, ytop = -40, border = NA, col ='white')
-  rect(xleft = 52, xright = 80, ybottom = -60, ytop = 40, border = NA, col ='white')
-  
-  mtext(side = 3, text = paste(simpleCap(as.character(dts[tp,"month_name"]))," ",as.character(dts[tp,"dom"]),", ", as.character(dts[tp,"year"]), sep = "" ) ,cex=0.25,line = -0.5) 
-}
-
-
-dev.off()
 
 
